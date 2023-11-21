896372ae9c6f2f4ff8acdd82df99495a52e5b0f7
Gustavo Bogarín <gbogarin@outlook.com>
2023-11-21 15:08:53 -0300

    Google integration.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,24 +37,10 @@
 		'green-screen'
 	];
 
-	let fbq: any;
-
 	onMount(() => {
 		const index = languages.findIndex((l) => navigator.language.includes(l.lang));
 		selectedLang = index >= 0 ? index : 0;
 		ready = true;
-
-		fbq = addPixel(
-			window,
-			document,
-			'script',
-			'https://connect.facebook.net/en_US/fbevents.js',
-			null,
-			null,
-			null
-		);
-		fbq('init', '6953039878052589');
-		fbq('track', 'PageView');
 
 		setTimeout(() => {
 			gsap.from('.contact', {
@@ -142,31 +128,10 @@
 	});
 
 	function mailto() {
-		fbq('track', 'Contact');
 		window.location.href = `mailto:${email}?subject=${
 			subject || 'I need something'
 		}&body=${name} here. ${message}`;
 	}
-
-	const addPixel = function (f: any, b: any, e: any, v: any, n: any, t: any, s: any): any {
-		if (f.fbq) return f.fbq;
-
-		n = f.fbq = function () {
-			n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-		};
-		if (!f._fbq) f._fbq = n;
-		n.push = n;
-		n.loaded = !0;
-		n.version = '2.0';
-		n.queue = [];
-		t = b.createElement(e);
-		t.async = !0;
-		t.src = v;
-		s = b.getElementsByTagName(e)[0];
-		s.parentNode.insertBefore(t, s);
-
-		return n;
-	};
 </script>
 
 <div class="md:w-[40%] md:w-[60%] md:w-[50%]" />
